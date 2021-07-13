b5d966f6235665d95c6b14917dfa6bce060e2d8e
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-07-13 10:08:11 -0400

    commit tree 6a30bd6e71fa1e1298e84cc33050516d457f4d31

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 function findMatching(array, name) {
      return array.filter(element => element.toUpperCase() === name.toUpperCase())
 }
-
 function fuzzyMatch(array, letters) {
-    return array.filter(element => element.slice(0,3) === letters)
+    return array.filter(element => element.slice(0,letters.length).toUpperCase() === letters.toUpperCase())
 }
+function matchName(array, string) {
+    return array.filter(object => object.name === string)
 
+}
 
