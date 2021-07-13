c13aae87036c0f9785f593412b5e00c19cb85e6b
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-07-13 09:50:44 -0400

    asked for help

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function findMatching (array, "string") {
-    if (array.includes("string")) {
-        return 
-    }
-    
+//const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
+//const result = words.filter(word => word.length > 6);
+
+function findMatching(array, name) {
+     return array.filter(element => element.toUpperCase() === name.toUpperCase())
 }
+
+function fuzzyMatch(array, letters) {
+    return array.filter(element => element.slice(0,3) === letters)
+}
+
 
